d95c121b97d26b7048fbacf8559e61408eae0203
Bedirhan Kelez <103532330+BedirK@users.noreply.github.com>
2024-02-21 21:59:25 +0300

    Update Q&A Sessions - Part 01.sql

--- Data-Analytics-Bootcamp/Q&A Sessions - Part 01.sql
+++ Data-Analytics-Bootcamp/Q&A Sessions - Part 01.sql
@@ -5,24 +5,24 @@ SELECT EmployeeID, LastName, FirstName, City
 FROM Employees
 WHERE City = 'Tacoma' OR City = 'London'
  
---Soru: CategoryId adetlerini ayrı ayrı hesaplanyınız (ürünler)
+--Soru: CategoryId adetlerini ayrı ayrı hesaplayınız (ürünler)
 --Calculate number of CategoryId quantities separately (products)
 
 SELECT CategoryID,
-COUNT (CategoryID) adet
+COUNT (CategoryID) AS #quantity
 FROM Products
 GROUP BY CategoryID
 
 --Soru: CategoryId bazında min , max ve ortalama birim fiyatlarını hesaplayınız (Max fiyatlara göre yüksek fiyattan düşük fiyata sıralanması)
 --Calculate min, max and average unit prices based on CategoryId ((Sorting from high price to low price according to max prices)
 
-SELECT CategoryID, MIN(UnitPrice) min_br_fyt, MAX(UnitPrice) max_br_fyt, AVG(UnitPrice) ort_br_fyt
+SELECT CategoryID, MIN(UnitPrice) min_unit_price, MAX(UnitPrice) max_unit_price, AVG(UnitPrice) avg_unit_price
 FROM Products
 GROUP BY CategoryID
 ORDER BY MAX (UnitPrice) DESC
 
---Soru: Customer tablosunundan ContactName, Orders tablosundan OrderID’ı çekiniz
---Question: Provide ContactName from the Customer table and OrderID from the Orders table
+--Soru: Customers tablosunundan ContactName, Orders tablosundan OrderID’ı çekiniz
+--Question: Provide ContactName from the Customers table and OrderID from the Orders table
 
 SELECT C.ContactName, O.OrderID FROM Customers C
 LEFT JOIN Orders O
@@ -31,17 +31,17 @@ ON C.CustomerID = O.CustomerID
 --Soru: Orders tablosunundan OrderID ile Customers tablosundan ContactName’i birleşmiş bir tablodan çekiniz
 --Question: Provide OrderID from the Orders table and ContactName from the Customers table with joined table
 
-SELECT O.OrderID, C.ContactName, COUNT(*)
+SELECT O.OrderID, C.ContactName
 FROM Orders O
 INNER JOIN Customers C
-ON O.CustomerID = C.CustomerID   --GROUP BY  C.ContactName , OrderID having COUNT(*)>1   //// TO CHECK DUPLICATE or ERRORS
+ON O.CustomerID = C.CustomerID   --GROUP BY  C.ContactName , OrderID having COUNT(*)>1   //// -TO CHECK DUPLICATE or ERRORS
 
 --Soru: Ürün ve Şirket adlarını listeleyin
 --List for Products and Company names
 
-SELECT P.ProductName UrunAdi, S.CompanyName SirketIsmi
+SELECT P.ProductName, S.CompanyName
 FROM Products P 
-LEFT JOIN Suppliers S 
+LEFT JOIN Suppliers S
 ON P.SupplierID = S.SupplierID
 
 --Soru: Ürün ve kategori adlarını listeleyin
@@ -72,27 +72,21 @@ ON P.SupplierID = S.SupplierID) A
 GROUP BY A.SirketIsmi, A.UrunAdi
 
 --ALTERNATIVE SOLUTION
-SELECT P.ProductName UrunAdi, COUNT(ProductName), S.CompanyName SirketIsmi, COUNT(CompanyName)
+SELECT P.ProductName , COUNT(ProductName) #Products, S.CompanyName , COUNT(CompanyName) #Companies
 FROM Products P 
 LEFT JOIN Suppliers S 
 ON P.SupplierID = S.SupplierID
 GROUP BY ProductName, CompanyName
 
---Soru: Kategori bazında toplam fiyatın hesaplayınız ve toplam birim fiyatı 200'den düşük kategorilerin filtreleyiniz
+--Soru: Kategori bazında toplam fiyatı hesaplayınız ve toplam birim fiyatı 200'den düşük kategorilerin filtreleyiniz
 --Calculate the total price for each category and filter out the categories that have a total unit price of less than $200.
 
-SELECT * FROM
-(SELECT CategoryID, SUM(UnitPrice) toplam_br_fyt
+SELECT CategoryName, SUM(UnitPrice) AS total_unit_price
 FROM Products
-GROUP BY CategoryID) A 
-WHERE toplam_br_fyt < 200
-
---ALTERNATIVE SOLUTION
- 
-SELECT CategoryID, SUM(UnitPrice) toplam_br_fyt
-FROM Products
-GROUP BY CategoryID
-HAVING SUM(UnitPrice) < 200
+INNER JOIN Categories 
+ON Products.CategoryID = Categories.CategoryID
+GROUP BY CategoryName
+HAVING SUM(UnitPrice) < 200;
 
 --Soru: En ucuz 5 ürünün ortalama fiyatı nedir ?
 --What is the average price of the 5 cheapest products?
@@ -131,16 +125,16 @@ ORDER BY UnitPrice ASC) B
 --Soru: Zamanında teslim edemediğim siparişlerim ID’leri nelerdir ve kaç gün geç gönderildi?
 --What are the IDs of the orders that were not shipped on time and how many days late were they shipped?
 
-SELECT OrderID, RequiredDate, ShippedDate, DATEDIFF(DAY, ShippedDate, RequiredDate) GecikmeSuresi
+SELECT OrderID, RequiredDate, ShippedDate, DATEDIFF(DAY, ShippedDate, RequiredDate) AS DelayTime
 FROM Orders     
 WHERE DATEDIFF(DAY, ShippedDate, RequiredDate) > 0 
-ORDER BY GecikmeSuresi DESC
+ORDER BY DelayTime DESC
 
 --"Geciken Urunler"in ortalama Kac gun Geciktiğini bulan sorguyu yazınız.
 --Write the query that will find the average number of days that products are late for delivery.
 
-SELECT OrderID, AVG(GecikmeSuresi) OrtalamaGecikmeSuresi FROM
-(SELECT OrderID, RequiredDate, ShippedDate, DATEDIFF(DAY, ShippedDate, RequiredDate) GecikmeSuresi
+SELECT OrderID, AVG(DelayTime) AS AvgDelayTime FROM
+(SELECT OrderID, RequiredDate, ShippedDate, DATEDIFF(DAY, ShippedDate, RequiredDate) AS DelayTime
 FROM Orders     
 WHERE DATEDIFF(DAY, ShippedDate, RequiredDate) > 0 ) A 
 GROUP BY OrderID
@@ -148,8 +142,8 @@ GROUP BY OrderID
 --"Erken Giden Urunlerin" ortalama kac gun erken gittiğini bulan sorguyu yazınız.
 --Write the query that finds the average number of days early delivery of products.
 
-SELECT OrderID, AVG(ErkenTeslimSuresi) OrtalamaErkenTeslimSuresi FROM
-(SELECT OrderID, RequiredDate, ShippedDate, DATEDIFF(DAY, RequiredDate, ShippedDate) ErkenTeslimSuresi
+SELECT OrderID, AVG(EarlyDeliveryTime) AS AvgEarlyDeliveryTime FROM
+(SELECT OrderID, RequiredDate, ShippedDate, DATEDIFF(DAY, RequiredDate, ShippedDate) AS EarlyDeliveryTime
 FROM Orders     
 WHERE DATEDIFF(DAY, RequiredDate, ShippedDate) > 0 ) B
 GROUP BY OrderID
